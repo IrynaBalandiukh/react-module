efb67c9d1254ceb3aca98a0451f2598eb7caad7c
IrunaBalandiukh <balandiukh.i@gmail.com>
2022-01-15 23:04:50 +0200

    hw1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Users from './Components/Users/Users';
+import Comments from './Components/Comments/Comments';
+import Posts from './Components/Posts/Posts';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
 
+        <div className='wrapper'>
+            <Users/>
+            <Posts/>
+        </div>
+
+        <hr/>
+        <Comments/>
     </div>
   );
 }
