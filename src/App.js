a6751ed874079dd03b491067bd8762676ba78f82
IrunaBalandiukh <balandiukh.i@gmail.com>
2022-01-17 22:35:12 +0200

    hw2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
+import {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
+import Users from './components/Users/Users';
+import UserDetails from './components/UserDetails/UserDetails';
+import Posts from './components/Posts/Posts';
+import css from './App.module.css';
 
-    </div>
-  );
+const App = () => {
+    const [user, setUser] = useState(null);
+    const [userId, setUserId] = useState(null);
+
+    const getUser = (user) => {
+        setUser(user)
+        setUserId(null)
+
+    };
+    const getUserId = (id) => {
+        setUserId(id)
+    }
+
+    return (
+        <div>
+            <div className={css.wrap}>
+                <Users getUser={getUser}/>
+                {user && <UserDetails user={user} getUserId={getUserId}/>}
+            </div>
+            {userId && <Posts userId={userId}/>}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from 'react';
+
+import {postService} from '../../services/post.service';
+import Post from '../Post/Post';
+
+const Posts = ({userId}) => {
+    const [posts, setPosts] = useState([]);
+    useEffect(()=>{
+        postService.getByUserId(userId).then(value => setPosts(value))
+    },[userId])
+
+    return (
+        <div>
+            {posts.map(post => <Post key={post.id} post={post}/>)}
+        </div>
+    );
+};
+
+export default Posts;
